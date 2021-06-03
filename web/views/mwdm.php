c180842af3e78b83709dbb72395e4b5f20818f43
MayWadyMin <capital.maywadymin@gmail.com>
2021-06-03 14:37:05 +0630

    testing mwdm

--- web/views/mwdm.php
+++ web/views/mwdm.php
@@ -1,6 +1,1 @@
-hello momo!
-
-Hello World!
-
-
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore iusto repudiandae laborum facere quibusdam odit voluptatum dolore omnis animi odio, temporibus soluta saepe eligendi sed, cumque, deserunt dicta. Sequi, magni.
+PHP Team
